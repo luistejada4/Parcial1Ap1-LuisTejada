c7b4d322ac61936a1043fcb2cd659cf6cc727ce6
Luis Tejada <Luis Tejada@LUISTEJADA-PC>
2017-02-13 18:34:35 -0400

    Add project files.

---
+++ Parcial1Ap1-LuisTejada/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Parcial1Ap1_LuisTejada
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
